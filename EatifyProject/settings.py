0c51f6355eefd80d2696090ae0a4385b06db7c1d
Raj-128 <rajpatil052003@gmail.com>
2026-06-04 19:24:07 +0530

    Test image fix

--- EatifyProject/settings.py
+++ EatifyProject/settings.py
@@ -39,7 +39,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 # Application definition
